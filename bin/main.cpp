859a61cebc4359c09307e27478d85f5bdd612dd4
GeorgiiChertkoev <geochert13028@gmail.com>
2024-12-10 22:48:44 +0300

    fixed main.cpp

--- bin/main.cpp
+++ bin/main.cpp
@@ -6,35 +6,35 @@
 
 #include "lib/game/game.h"
 
-int main() {
+
+void GameSample() {
     Game a;
-    // a.Load("/home/geo/prog/labs/labwork5-GeorgiiChertkoev/input.txt");
-    // a.CreateMaster();
     a.SetWidth(5);
     a.SetHeight(4);
     a.SetCount(3, 1);
     a.SetCount(2, 2);
     a.SetCount(1, 1);
-    std::cout << a.Shoot() << '\n';
+    a.Start();
+    // std::cout << a.Shoot() << '\n';
+    a.Shoot();
     a.SetResult("hit");
-    std::cout << a.Shoot() << '\n';
+    // std::cout << a.Shoot() << '\n';
+    a.Shoot();
     a.SetResult("miss");
-    std::cout << a.Shoot() << '\n';
+    // std::cout << a.Shoot() << '\n';
+    a.Shoot();
     a.SetResult("hit");
     a.DrawEnemyField("before_kill.txt");
-    std::cout << a.Shoot() << '\n';
+    // std::cout << a.Shoot() << '\n';
+    a.Shoot();
     a.SetResult("kill");
-    // std::cout << a.Shoot() << '\n';
-    // a.SetResult("hit");
-    // std::cout << a.Shoot() << '\n';
-    // a.SetResult("hit");
+}
 
-    a.DrawEnemyField("after.txt");
+
+int main() {
+    Game a;
+
     a.Play();
-
-    
-    a.DrawEnemyField("enemy.txt");
-    
     
     return 0;
 }
